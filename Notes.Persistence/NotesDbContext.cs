0ff0e7660cd7d848a72b180796ed96e15bcac1aa
Av0ff <vingaardium@gmail.com>
2023-02-14 16:39:48 +0500

    Add: DataBase Layer

---
+++ Notes.Persistence/NotesDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+using Notes.Persistence.EntityTypeConfigurations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notes.Persistence
+{
+	internal class NotesDbContext : DbContext, INotesDbContext
+	{
+		public NotesDbContext(DbContextOptions<NotesDbContext> options)
+			:base(options)
+		{
+			Database.EnsureCreated();
+		}
+
+		public DbSet<Note> Notes { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.ApplyConfiguration(new NoteConfiguration());
+		}
+	}
+}
