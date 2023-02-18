2234eda9a34b382cbe2c5198eca77ec7a9248c52
Av0ff <vingaardium@gmail.com>
2023-02-19 04:07:24 +0500

    Add: Web API

---
+++ Notes.WebAPI/Models/CreateNoteViewModel.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Notes.Application.Common.Mappings;
+using Notes.Application.Notes.Commands.CreateNote;
+
+namespace Notes.WebAPI.Models
+{
+	public class CreateNoteViewModel : IMapWith<CreateNoteCommand>
+	{
+		public string Title { get; set; }
+		public string Description { get; set; }
+
+		public void Mapping(Profile profile)
+		{
+			profile.CreateMap<CreateNoteViewModel, CreateNoteCommand>();
+		}
+	}
+}
---
+++ Notes.WebAPI/Models/UpdateNoteViewModel.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Notes.Application.Common.Mappings;
+using Notes.Application.Notes.Commands.UpdateNote;
+using System;
+
+namespace Notes.WebAPI.Models
+{
+	public class UpdateNoteViewModel : IMapWith<UpdateNoteCommand>
+	{
+		public Guid Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		
+		public void Mapping(Profile profile)
+		{
+			profile.CreateMap<UpdateNoteViewModel, UpdateNoteCommand>();
+		}
+	}
+}
