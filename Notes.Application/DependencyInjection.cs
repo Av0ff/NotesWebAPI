35ed9ce761a3340c32f36dbc5e3688b5fc4e3612
Av0ff <vingaardium@gmail.com>
2023-02-18 18:40:03 +0500

    Add: Dependency Injection
    Database, MediatR

---
+++ Notes.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Notes.Application
+{
+	public static class DependencyInjection
+	{
+		public static IServiceCollection AddApplication(this IServiceCollection services)
+		{
+			services.AddMediatR(config => config.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+			return services;
+		}
+	}
+}
---
+++ Notes.Persistence/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Notes.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notes.Persistence
+{
+	public static class DependencyInjection
+	{
+		public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+		{
+			var connection = configuration.GetConnectionString("MSSQL");
+			services.AddDbContext<NotesDbContext>(opt => opt.UseSqlServer(connection));
+			services.AddScoped<INotesDbContext, NotesDbContext>();
+			return services;
+		}
+	}
+}
